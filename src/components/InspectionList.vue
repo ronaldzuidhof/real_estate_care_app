b20dd555f1741de5ffd2de88be3e9d1cb3fcc5fe
Ronald Zuidhof <ronaldzuidhof@gmail.com>
2024-02-27 20:46:05 +0100

    create methods for v-data-table (vuetify)

--- src/components/InspectionList.vue
+++ src/components/InspectionList.vue
@@ -1,6 +1,11 @@
 <template lang="">
     <section>
-        <v-data-table :items="items"></v-data-table>
+        <v-data-table 
+        :headers="headers"
+        :items="inspections"
+        item-key="value"
+        items-per-page="5"
+        ></v-data-table>
     </section>
 </template>
 <script>
@@ -12,19 +17,50 @@ export default {
     name: "InspectionList",
     data() {
         return{
-            items: reportData
+            headers: [
+                {title: "ID", allign: "start", sortable: false, value: 'id'},
+                {title: "Stad", allign: "start", sortable: true, value: 'city'},
+                {title: "Straatnaam", allign: "start", sortable: false, value: 'streetName'},
+                {title: "Huisnummer", allign: "start", sortable: false, value: 'houseNumber'},
+                {title: "Inspectie datum", allign: "start", sortable: true, value: 'inspectionDate'},
+                {title: "Inspectie tijd", allign: "start", sortable: true, value: 'inspectionDate'},
+            ],
+            inspections: [],
         }
     }, 
     methods: {
+        // function to convert date ISO format to defined date format
         dateConversion(dateIsoFormat){
             return moment(dateIsoFormat).format('DD-MM-YYYY');
         },
+        // function to convert date ISO format to defined time format
         timeConversion(dateIsoFormat){
             return moment(dateIsoFormat).format('HH:mm');
         },
-        getHeadersJson(){
-            
+        // function to extract the inspection objects from the JSON object (Javascript)
+        extractJsonInspections(json, headers){
+            // loop trough json file
+            for (let i = 0; i < json.length; i++){
+                // create empty inspection object
+                let inspection = new Object();
+                // Loop trough headers array and create key/value pairs in inspection object
+                for( let j = 0; j < headers.length; j++){
+                    let key = headers[j].value
+                    if (key === "inspectionDate"){
+                        inspection[key] = this.dateConversion(json[i][key])
+                        inspection["inspectionTime"] = this.timeConversion(json[i][key])
+                    } else {
+                        inspection[key] = json[i][key]; 
+                    }
+                }
+                // Add inspection objects to inspections Array
+                this.inspections.push(inspection);
+            } 
         }
+    },
+    created(){
+        // Call function extractJsonInspections on reportData
+        this.extractJsonInspections(reportData, this.headers)
     }
 }
 
